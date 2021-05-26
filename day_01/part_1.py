b660d18cb9720e4afbd5b7f933ea6a9436217e04
AndreaBruno97 <andbruno97@gmail.com>
2021-05-26 16:42:30 +0200

    Day 1

--- day_01/part_1.py
+++ day_01/part_1.py
@@ -2,3 +2,8 @@
 filename = 'input.txt'
 with open(filename) as f:
     content = f.read()
+
+masses = [int(x) for x in content.split("\n")]
+total = sum([int(x/3)-2 for x in masses])
+
+print("The sum of the fuel requirements is: " + str(total))
--- day_01/part_2.py
+++ day_01/part_2.py
@@ -1,4 +1,19 @@
+def compute_fuel(start_fuel):
+    total_fuel = 0
+    cur_delta = start_fuel
+
+    while cur_delta > 0:
+        cur_delta = int(cur_delta/3) - 2
+        if cur_delta > 0: total_fuel += cur_delta
+
+    return total_fuel
+
 ''' Open file '''
 filename = 'input.txt'
 with open(filename) as f:
     content = f.read()
+
+masses = [int(x) for x in content.split("\n")]
+total = sum([compute_fuel(x) for x in masses])
+
+print("The sum of the fuel requirements is: " + str(total))
