13f618465b0048e17bf47f9c548ad9ef9c006c8c
AndreaBruno97 <andbruno97@gmail.com>
2021-05-26 21:09:52 +0200

    Day 4

--- day_04/part_1.py
+++ day_04/part_1.py
@@ -2,3 +2,16 @@
 filename = 'input.txt'
 with open(filename) as f:
     content = f.read()
+
+total = 0
+for i in range(137683, 596254):
+    cur_num = [int(x) for x in str(i)]
+    if not sum([cur_num[i - 1] == cur_num[i] for i in range(1, len(cur_num))]):
+        continue
+
+    if sum([cur_num[i - 1] > cur_num[i] for i in range(1, len(cur_num))]):
+        continue
+
+    total+=1
+
+print("There total number of values that satisfy the request is: " + str(total))
--- day_04/part_2.py
+++ day_04/part_2.py
@@ -2,3 +2,28 @@
 filename = 'input.txt'
 with open(filename) as f:
     content = f.read()
+
+total = 0
+for i in range(137683, 596254):
+    cur_num = [int(x) for x in str(i)]
+    if not sum([cur_num[i - 1] == cur_num[i] for i in range(1, len(cur_num))]):
+        continue
+
+    if sum([cur_num[i - 1] > cur_num[i] for i in range(1, len(cur_num))]):
+        continue
+
+    last = cur_num[0]
+    count = 1
+    found = False
+    for i in range(1, len(cur_num)):
+        if cur_num[i] == last:
+            count+=1
+        else:
+            if count == 2:
+                found = True
+            last = cur_num[i]
+            count = 1
+    if found or count == 2:
+        total+=1
+
+print("There total number of values that satisfy the request is: " + str(total))
