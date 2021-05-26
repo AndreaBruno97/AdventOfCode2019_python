7fb6d25e3ab16a29f37cf000b53b3b2b24ff7cc4
AndreaBruno97 <andbruno97@gmail.com>
2021-05-26 17:08:14 +0200

    Day 2

--- day_02/part_1.py
+++ day_02/part_1.py
@@ -2,3 +2,23 @@
 filename = 'input.txt'
 with open(filename) as f:
     content = f.read()
+program = [int(x) for x in content.split(",")]
+ADD = 1
+MULTIPLY = 2
+STOP = 99
+
+program[1] = 12
+program[2] = 2
+
+index = 0
+while program[index] != STOP:
+    opcode, a, b, c = program[index:index+4]
+
+    if opcode == ADD:
+        program[c] = program[a] + program[b]
+    else:
+        program[c] = program[a] * program[b]
+
+    index += 4
+
+print("The value left at position 0 is: " + str(program[0]))
--- day_02/part_2.py
+++ day_02/part_2.py
@@ -2,3 +2,34 @@
 filename = 'input.txt'
 with open(filename) as f:
     content = f.read()
+program = [int(x) for x in content.split(",")]
+ADD = 1
+MULTIPLY = 2
+STOP = 99
+TARGET = 19690720
+RANGE = 100
+
+original_program = program.copy()
+
+for noun in range(RANGE):
+    for verb in range(RANGE):
+        program = original_program.copy()
+
+        program[1] = noun
+        program[2] = verb
+
+        index = 0
+        while program[index] != STOP:
+            opcode, a, b, c = program[index:index + 4]
+
+            if opcode == ADD:
+                program[c] = program[a] + program[b]
+            else:
+                program[c] = program[a] * program[b]
+
+            index += 4
+
+        if program[0] == TARGET: break
+    if program[0] == TARGET: break
+
+print("100 * noun + verb: " + str((100 * noun) + verb))
