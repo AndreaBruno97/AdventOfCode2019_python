992d56823d2171f74fddd78ec0975494f02740ea
AndreaBruno97 <andbruno97@gmail.com>
2021-05-27 22:31:59 +0200

    Day 6

--- day_06/part_1.py
+++ day_06/part_1.py
@@ -1,4 +1,42 @@
+class Planet:
+    def __init__(self, name):
+        self.name = name
+        self.children = []
+        self.level = 0
+
+def assign_level(planets, parent):
+    for child in planets[parent].children:
+        planets[child].level = planets[parent].level + 1
+        planets = assign_level(planets, child)
+    return planets
+
 ''' Open file '''
 filename = 'input.txt'
 with open(filename) as f:
     content = f.read()
+
+planets = {}
+
+for line in content.split("\n"):
+    parent_name, cur_planet_name = line.split(")")
+
+    if parent_name in planets:
+        parent = planets[parent_name]
+    else:
+        parent = Planet(parent_name)
+        planets[parent_name] = parent
+
+    if cur_planet_name in planets:
+        new_planet = planets[cur_planet_name]
+    else:
+        new_planet = Planet(cur_planet_name)
+        planets[cur_planet_name] = new_planet
+
+    parent.children.append(cur_planet_name)
+
+center_of_mass = "COM"
+planets[center_of_mass].level = 0
+planets = assign_level(planets, center_of_mass)
+total_orbits = sum([x.level for x in planets.values()])
+
+print("The total number of direct and indirect orbits is: " + str(total_orbits))
--- day_06/part_2.py
+++ day_06/part_2.py
@@ -1,4 +1,53 @@
+CENTER_OF_MASS = "COM"
+SANTA = "SAN"
+YOU = "YOU"
+
+
+class Planet:
+    def __init__(self, name):
+        self.name = name
+        self.parent = ""
+
+
 ''' Open file '''
 filename = 'input.txt'
 with open(filename) as f:
     content = f.read()
+
+planets = {}
+
+for line in content.split("\n"):
+    parent_name, cur_planet_name = line.split(")")
+
+    if parent_name in planets:
+        parent = planets[parent_name]
+    else:
+        parent = Planet(parent_name)
+        planets[parent_name] = parent
+
+    if cur_planet_name in planets:
+        new_planet = planets[cur_planet_name]
+    else:
+        new_planet = Planet(cur_planet_name)
+        planets[cur_planet_name] = new_planet
+
+    new_planet.parent = parent_name
+
+santa_orbital_chain = [SANTA]
+i = 0
+while santa_orbital_chain[i] != CENTER_OF_MASS:
+    santa_orbital_chain.append(planets[santa_orbital_chain[i]].parent)
+    i += 1
+
+i = 1
+common_ancestor = YOU
+while common_ancestor != CENTER_OF_MASS and common_ancestor not in santa_orbital_chain:
+    common_ancestor = planets[common_ancestor].parent
+    i += 1
+
+santa_orbital_transfers_num = santa_orbital_chain.index(common_ancestor) - 1
+you_orbital_transfers_num = i - 2
+
+total_transfers = santa_orbital_transfers_num + you_orbital_transfers_num
+
+print("The total number of orbital transfers is: " + str(total_transfers))
