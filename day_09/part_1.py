ac30b1a27f213206f717c2a8c6ed457fa2de248a
AndreaBruno97 <andbruno97@gmail.com>
2021-05-28 14:59:10 +0200

    Day 9

--- day_09/part_1.py
+++ day_09/part_1.py
@@ -1,4 +1,11 @@
+import intcode_computer as intcode
+
 ''' Open file '''
 filename = 'input.txt'
 with open(filename) as f:
     content = f.read()
+
+computer = intcode.IntcodeComputer(content)
+result = computer.execute_program(1)[0]
+
+print("The BOOST keycode is: " + str(result))
--- day_09/part_2.py
+++ day_09/part_2.py
@@ -1,4 +1,11 @@
+import intcode_computer as intcode
+
 ''' Open file '''
 filename = 'input.txt'
 with open(filename) as f:
     content = f.read()
+
+computer = intcode.IntcodeComputer(content)
+result = computer.execute_program(2)[0]
+
+print("The coordinate is: " + str(result))
