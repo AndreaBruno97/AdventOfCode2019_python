c5411cae1a5e040e2ff84003a9d23867c07058c3
AndreaBruno97 <andbruno97@gmail.com>
2021-05-28 13:55:13 +0200

    Day 8

--- day_08/part_1.py
+++ day_08/part_1.py
@@ -1,4 +1,22 @@
+import numpy as np
+
+ROWS = 6
+COLS = 25
+
 ''' Open file '''
 filename = 'input.txt'
 with open(filename) as f:
     content = f.read()
+
+image = [int(x) for x in content]
+
+num_layers = int(len(image) / (ROWS * COLS))
+layers = np.array(image).reshape((num_layers, ROWS, COLS))
+
+min_zeros_layer = sorted(layers, key=lambda x: np.count_nonzero(x == 0))[0]
+num_1 = np.count_nonzero(min_zeros_layer == 1)
+num_2 = np.count_nonzero(min_zeros_layer == 2)
+
+total = num_1 * num_2
+
+print("The total value is: " + str(total))
--- day_08/part_2.py
+++ day_08/part_2.py
@@ -1,4 +1,28 @@
+import numpy as np
+import textwrap
+
+ROWS = 6
+COLS = 25
+
+BLACK = u"\u25A0"
+WHITE = " "
+
+
 ''' Open file '''
 filename = 'input.txt'
 with open(filename) as f:
     content = f.read()
+
+image = [int(x) for x in content]
+
+num_layers = int(len(image) / (ROWS * COLS))
+layers = np.array(image).reshape((num_layers, ROWS, COLS))
+
+layers = np.moveaxis(layers, 0, -1).reshape(ROWS*COLS, num_layers)
+
+message_array = [x[x != 2][0] for x in layers]
+message_array = [BLACK if x else WHITE for x in message_array]
+message = np.array(message_array).reshape(ROWS, COLS)
+
+print("The final message is: ")
+[print(textwrap.fill(str(x), width=200)) for x in message]
