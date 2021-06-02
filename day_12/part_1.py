d96f4d6d269592320fbc4f9c01bf34b3055dac11
AndreaBruno97 <andbruno97@gmail.com>
2021-06-02 22:36:18 +0200

    Day 12, part 1

--- day_12/part_1.py
+++ day_12/part_1.py
@@ -1,4 +1,42 @@
+import re
+import numpy as np
+
+MOON_NUM = 4
+D_NUM = 3
+STEPS_NUM = 1000
+
 ''' Open file '''
 filename = 'input.txt'
 with open(filename) as f:
     content = f.read()
+
+speeds = [[0 for x in range(D_NUM)] for y in range(MOON_NUM)]
+positions = [
+    [int(x) for x in re.sub(r'[<> xyz=]', '', line).split(",")]
+    for line in content.split("\n")
+]
+
+for i in range(STEPS_NUM):
+    # Apply gravity
+    for moon in range(MOON_NUM):
+        for dir in range(D_NUM):
+            speeds[moon][dir] += sum(
+                                    np.sign([
+                                          other_moon_pos[dir] - positions[moon][dir]
+                                        for other_moon_pos in positions
+                                    ]))
+    # Apply speed
+    for moon in range(MOON_NUM):
+        for dir in range(D_NUM):
+            positions[moon][dir] += speeds[moon][dir]
+
+energy = sum([
+    sum(
+        [abs(x) for x in positions[moon]]
+    ) * sum(
+        [abs(x) for x in speeds[moon]]
+    )
+    for moon in range(MOON_NUM)
+])
+
+print("The total energy of the system is: " + str(energy))
--- day_12/part_2.py
+++ day_12/part_2.py
@@ -1,4 +1,51 @@
+import re
+import numpy as np
+
+MOON_NUM = 4
+D_NUM = 3
+
 ''' Open file '''
 filename = 'input.txt'
 with open(filename) as f:
     content = f.read()
+
+initial_speeds = [[0 for x in range(D_NUM)] for y in range(MOON_NUM)]
+initial_positions = [
+    [int(x) for x in re.sub(r'[<> xyz=]', '', line).split(",")]
+    for line in content.split("\n")
+]
+
+positions = [pos.copy() for pos in initial_positions]
+speeds = [sp.copy() for sp in initial_speeds]
+
+steps_num = 0
+
+while True:
+    steps_num += 1
+
+    # Apply gravity
+    for moon in range(MOON_NUM):
+        for dir in range(D_NUM):
+            speeds[moon][dir] += sum(
+                                    np.sign([
+                                          other_moon_pos[dir] - positions[moon][dir]
+                                        for other_moon_pos in positions
+                                    ]))
+    # Apply speed
+    for moon in range(MOON_NUM):
+        for dir in range(D_NUM):
+            positions[moon][dir] += speeds[moon][dir]
+
+    if not steps_num%1000:
+        print(str(steps_num))
+
+    if not sum([
+        positions[moon] != initial_positions[moon]
+        for moon in range(MOON_NUM)
+    ]) and not sum([
+                speeds[moon] != initial_speeds[moon]
+            for moon in range(MOON_NUM)
+    ]):
+        break
+
+print("The number of steps is: " + str(steps_num))
